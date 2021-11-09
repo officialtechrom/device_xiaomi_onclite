c99092f7bcab0e702972bec8cf0779a512b4904c
officialtechrom <sharmaaniket022@gmail.com>
2021-11-09 23:13:36 +0530

    onclite: Bringup For Awaken12

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_onclite.mk
+    $(LOCAL_DIR)/awaken_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+	awaken_onclite-user \
+	awaken_onclite-userdebug \
+	awaken_onclite-eng
